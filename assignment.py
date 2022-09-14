216b7e1670bff94b0bb7c7dc29b0a48aa5b42aba
licenzart <109023681+licenzart@users.noreply.github.com>
2022-09-14 14:12:30 +0800

    Update assignment.py

--- assignment.py
+++ assignment.py
@@ -4,3 +4,4 @@ import numpy as np
 df = pd.DataFrame([["Iris-setosa"], ["Iris-versicolor"],["Iris-virginica"]], columns=['Iris type'], index=['0', '1','2'])
 st.header("Final Assignment for beginner")
 st.table(df)
+st.snow()
