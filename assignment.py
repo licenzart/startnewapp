11b4d0a7afde150bf01672dfa97adadd8ac9b56e
licenzart <109023681+licenzart@users.noreply.github.com>
2022-09-14 15:18:15 +0800

    Update assignment.py

--- assignment.py
+++ assignment.py
@@ -1,8 +1,15 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
+from PIL import Image
+
 df = pd.DataFrame([["Iris-setosa"], ["Iris-versicolor"],["Iris-virginica"]], columns=['Iris type'], index=['0', '1','2'])
 st.header("Final Assignment for beginner")
-st.info('This is a purely informational message', icon="ℹ️")
+st.info('Please becareful of the snowflakes!', icon="ℹ️")
 st.table(df)
 st.snow()
+
+
+image = Image.open('food1.jpg')
+
+st.image(image, caption='Food1')
