da2c7dee07f85de6d40aa3fcb8f9b0048dd87732
licenzart <109023681+licenzart@users.noreply.github.com>
2022-09-14 14:08:58 +0800

    Add files via upload

---
+++ assignment.py
@@ -0,0 +1,6 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+df = pd.DataFrame([["Iris-setosa"], ["Iris-versicolor"],["Iris-virginica"]], columns=['Iris type'], index=['0', '1','2'])
+st.header("Final Assignment for beginner")
+st.table(df)
